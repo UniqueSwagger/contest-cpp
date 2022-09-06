b5f98de582b5ff1fa2475c43ad80af6c71f7882c
UniqueSwagger <rahmanmahi02@gmail.com>
2022-09-06 20:32:33 +0600

    updated

--- howOld.cpp
+++ howOld.cpp
@@ -7,34 +7,28 @@ int main()
     cin >> t;
     for (int i = 0; i < t; i++)
     {
-        int d1, d2, m1, m2, y1, y2;
+        int currentDay, currentMonth, currentYear, birthDay, birthMonth, birthYear;
         char c;
-        cin >> d1 >> c >> m1 >> c >> y1;
-        cin >> d2 >> c >> m2 >> c >> y2;
-        int age = y1 - y2;
-        if (m1 < m2)
-        {
+        cin >> currentDay >> c >> currentMonth >> c >> currentYear;
+        cin >> birthDay >> c >> birthMonth >> c >> birthYear;
+        // calculate the age by subtracting the current year from the birth year
+        int age = currentYear - birthYear;
+        // if the current month is less than the birth month, then the person has not had their birthday yet
+        if (currentMonth < birthMonth)
             age--;
-        }
-        else if (m1 == m2)
+        // if the current month is the same as the birth month, then we need to check the day
+        else if (currentMonth == birthMonth)
         {
-            if (d1 < d2)
-            {
+            // if the current day is less than the birth day, then the person has not had their birthday yet
+            if (currentDay < birthDay)
                 age--;
-            }
         }
         if (age < 0)
-        {
             cout << "Case #" << i + 1 << ": Invalid birth date" << endl;
-        }
         else if (age > 130)
-        {
             cout << "Case #" << i + 1 << ": Check birth date" << endl;
-        }
         else
-        {
             cout << "Case #" << i + 1 << ": " << age << endl;
-        }
     }
 
     return 0;
