aa7a41dbd07660dc5a50b387c3dfef8a01160f1a
UniqueSwagger <rahmanmahi02@gmail.com>
2022-09-06 21:03:04 +0600

    updated

--- summingDigits.cpp
+++ summingDigits.cpp
@@ -8,23 +8,35 @@ int main()
     {
         if (n == 0)
         {
+            // if n is 0, then we are done
             break;
         }
-        int sum = 0;
+        int sum = 0; // initialize the sum to 0
         while (n > 0)
         {
             sum += n % 10;
             n /= 10;
+            // what will be the sum if n=47?
+            // sum = 0 + 47 % 10 = 7
+            // n= 47/10= 4
+            // sum= 7 + 4 % 10= 11
+            // n= 4/10= 0
         }
         while (sum > 9)
         {
-            int temp = 0;
+            // current sum is 11 which is greater than 9 so we need to sum the digits of 11
+            int ultimateSum = 0;
             while (sum > 0)
             {
-                temp += sum % 10;
+                ultimateSum += sum % 10;
                 sum /= 10;
+                // what will be the sum if sum=11?
+                // ultimateSum = 0 + 11 % 10 = 1
+                // sum= 11/10= 1
+                // ultimateSum = 1 + 1 % 10= 2
+                // sum= 1/10= 0
             }
-            sum = temp;
+            sum = ultimateSum;
         }
         cout << sum << endl;
     }
